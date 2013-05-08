1021bf05355124d3033e594bd4d9723136e6988b
AleSimonca <ale92_s@yahoo.ro>
2013-05-08 16:50:20 +0300

    FUN 14:View product details

--- mvc/app/controller/product.php
+++ mvc/app/controller/product.php
@@ -6,3 +6,18 @@
  * Time: 3:05 PM
  * To change this template use File | Settings | File Templates.
  */
+
+class controller_product {
+
+    /*
+     * Main product class
+     */
+    function action_index($params){
+        $id = $params[0];
+        if($product = model_product::load_by_id($id)){
+
+        }
+        // Include view for this page
+        @include_once APP_PATH . 'view/product_view.tpl.php';
+    }
+}
