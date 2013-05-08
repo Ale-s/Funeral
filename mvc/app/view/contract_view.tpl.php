4ebc2909b379cd6001eac7db14c9c9b177fdf6aa
cristina.sandor <crystynutza_maria@yahoo.com>
2013-05-08 16:51:58 +0300

    Added controller_contract and contract_view.

---
+++ mvc/app/view/contract_view.tpl.php
@@ -0,0 +1,6 @@
+<?php @include APP_PATH . 'view/snippets/header.tpl.php'; ?>
+<h2>Contracte</h2>
+
+<?php echo $con->id . '  ' . $con->date . '  '. $con->final_price . ' '; ?>
+
+<?php @include APP_PATH . 'view/snippets/footer.tpl.php';
