d77d0c30a0b1d9e832ff3630d72b534389cc53bb
Hera Octavian <remus.hera@gmail.com>
2013-05-08 17:13:34 +0300

    FUN-10 View product details.

--- mvc/app/controller/product.php
+++ mvc/app/controller/product.php
@@ -1,8 +1,19 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: ale
- * Date: 5/8/13
- * Time: 3:05 PM
- * To change this template use File | Settings | File Templates.
+ * Controller for handling users.
  */
+
+class controller_product {
+
+    /*
+     * Main product class
+     */
+
+    // Load a product from db and display information about him.
+    function action_view($params){
+        $product = model_product::load_by_id($params[0]);
+
+        // Include view for this page
+        @include_once APP_PATH . 'view/product_view.tpl.php';
+    }
+}
