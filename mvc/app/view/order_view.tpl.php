7daac1c2b38ce86993c90c030d2260e1f3e2e7b6
Horecica Vlad <vladcom80@yahoo.com>
2013-05-09 16:24:36 +0300

    FUN-34:Page view contract

--- mvc/app/view/order_view.tpl.php
+++ mvc/app/view/order_view.tpl.php
@@ -3,6 +3,6 @@
 <h2> Order information : </h2>
     <p>Order ID: <?php echo $order->id; ?></p>
     <p>Client ID: <?php echo $order->client_id; ?></p>
-    <p>Contract ID: <?php echo $order->contract_id; ?></p>
+    <p>Contract ID:<a href="<?php APP_PATH ?>/contract/view/ <?php echo $order->contract_id; ?>"><?php echo $order->contract_id; ?></a></p>
 
 <?php @include APP_PATH . 'view/snippets/footer.tpl.php';
