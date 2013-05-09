4772f06e0e41b489fb94e67a0bc65c956698916c
Sorina Puiac <sorina_puiac@yahoo.com>
2013-05-09 10:42:55 +0300

    FUN-13:Page: View all categories.

--- mvc/app/controller/category.php
+++ mvc/app/controller/category.php
@@ -2,7 +2,7 @@
     class controller_category {
 
         //Load all categories.
-        function action_load() {
+        function action_load(){
             $category = new model_category();
             $result = $category::load_all();
             @include_once APP_PATH . 'view/category_view.tpl.php';
