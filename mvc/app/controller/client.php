2be0d180361b8ab2a10ede986da715468de63a15
Horecica Vlad <vladcom80@yahoo.com>
2013-05-09 14:39:13 +0300

    FUN-12 Page: View client details reviewed

--- mvc/app/controller/client.php
+++ mvc/app/controller/client.php
@@ -6,7 +6,12 @@ class controller_client {
 
     function action_view($params) {
         $client = $params[0];
-        $client = model_client::load_by_id($client);
+        if (model_client::load_by_id($client)!=false){
+            $client = model_client::load_by_id($client);
+        }
+        else {
+            return false;
+        }
 
         // Include view for this page
         @include_once APP_PATH . 'view/client_view.tpl.php';
