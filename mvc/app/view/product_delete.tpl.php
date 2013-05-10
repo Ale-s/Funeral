cab34975b3ef2b22f48d3b4bef761016ba4fb315
AleSimonca <ale92_s@yahoo.ro>
2013-05-10 11:26:41 +0300

    FUN- 17:Product delete(view).

---
+++ mvc/app/view/product_delete.tpl.php
@@ -0,0 +1,12 @@
+<?php @include APP_PATH . 'view/snippets/header.tpl.php'; ?>
+
+<form action="<?php echo APP_URL; ?>product/deleteProduct/<?php echo $product->id; ?>" method="post">
+
+    <label>sigur stergeti produsul <?php echo $product->name ?></label></ br>
+
+    <input type="submit" name="form[action]" value="Delete" />
+
+    <a href = "<?php echo APP_URL; ?>product/listbycategory/<?php echo $product->category_id?>">Back</a><br />
+</form>
+
+<?php @include APP_PATH . 'view/snippets/footer.tpl.php'; ?>
