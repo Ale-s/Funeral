1d33a5a93ba9f2f48056f73540bfd08091ae3e25
Hera Octavian <remus.hera@gmail.com>
2013-05-10 16:31:26 +0300

    FUN-27 Page view cart (version II)

--- mvc/app/controller/cart.php
+++ mvc/app/controller/cart.php
@@ -7,14 +7,15 @@ class controller_cart {
     // Include the cart_view.tpl.php to display all products from $_SESSION['cart']
     public function action_view() {
 
-
         @include_once APP_PATH . 'view/cart_view.tpl.php';
     }
 
     // Delete a row from shopping cart
     public function action_deleteProduct() {
-        $id = $_POST['form']['id'];
-        unset($_SESSION['cart'][$id]);
+        $product_id = intval($_POST['form']['id']);
+        $quantity = intval($_POST['form']['quantity']);
+        model_cart::resetQuantity($product_id,$quantity);
+        unset($_SESSION['cart'][$product_id]);
         header('Location:'. APP_URL.'cart/view');
         die();
     }
--- mvc/app/model/cart.php
+++ mvc/app/model/cart.php
@@ -4,8 +4,21 @@
  */
 class model_cart {
 
+    // Add a product with a specific quantity in $_SESSION['cart'].
     public static function add ($product_id, $quantity) {
         $_SESSION['cart'][$product_id] = $quantity;
+    }
+
+    // Restore in database the old quantity when a product from $_SESSION['cart'] is deleted.
+    public static function resetQuantity ($product_id,$quantity) {
+        $db = model_database::instance();
+        $sql1 = "Select product_amount from product where product_id =". intval($product_id);
+        $row = $db->get_row($sql1);
+        $q = intval($row['product_amount']);
+        $old_quantity = intval($q + $quantity);
+        $sql = ' UPDATE product
+                    set product_amount = '. $old_quantity .' WHERE product_id = ' . intval($product_id);
+        $db->execute($sql);
     }
 
 
