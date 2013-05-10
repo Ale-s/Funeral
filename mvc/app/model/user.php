398b4701e0c63227b0641f00bb3fedb9900ef00a
cristina.sandor <crystynutza_maria@yahoo.com>
2013-05-10 17:50:26 +0300

    FUN-25 : Page my account .

--- mvc/app/model/user.php
+++ mvc/app/model/user.php
@@ -31,6 +31,22 @@ class model_user {
         return FALSE;
     }
 
+    public static function load_by_client_id($id) {
+        $db = model_database::instance();
+        $sql = 'SELECT *
+			FROM user
+			WHERE client_id = ' . "'" . $id . "'";
+        if ($result = $db->get_row($sql)) {
+            $user = new model_user;
+            $user->name = $result['user_name'];
+            $user->password = $result['user_password'];
+            $user->type = $result['user_type'];
+            $user->client_id = $result['client_id'];
+            return $user;
+        }
+        return FALSE;
+    }
+
     // Return a client with a specifically id
     public function get_client() {
         return model_client::load_by_id($this->client_id);
@@ -48,12 +64,13 @@ class model_user {
    public function create_user($name,$password, $id) {
        $db = model_database::instance();
 
-       $sql =  'Insert into user (user_name,user_password,client_id) values(\'' . mysql_real_escape_string($name) . '\',\'' . mysql_real_escape_string($password) . '\',\'' . intval($id) . '\')';
-
+       //$sql =  'Insert into user (user_name,user_password,user_type,client_id) values(\'' . mysql_real_escape_string($name) . '\',\'' . mysql_real_escape_string($password) . '\',\'' . intval($id) . '\')';
+        $sql = 'Insert into user (user_name,user_password,client_id,user_type) values("' . mysql_real_escape_string($name) . '","' . mysql_real_escape_string($password) . '","' . intval($id) . '",1)';
        if ($db->execute($sql)){
            return model_user::load_by_username($name);
        }
        return false;
    }
+
 
 }
